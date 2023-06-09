85a9dcb23ae74bbd76c5d02e2f4326c878c40e56
Annaisha <103822566+annxyfwnd@users.noreply.github.com>
2023-06-09 13:11:04 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const multer = require('multer');
+
+
+require('dotenv').config()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+
+const router = require("./routes/router")
+
+app.use('/api', router)
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
